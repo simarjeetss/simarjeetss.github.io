e06a97d0cbbdc4f465d60a01459bbb7640960431
“simarjeetss” <simarjeetss529@gmail.com>
2025-10-14 21:03:07 -0700

    fix: assets directoty mismatch with vercel directory

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -3,21 +3,19 @@ import Masonry from 'react-masonry-css';
 import Lightbox from 'yet-another-react-lightbox';
 import 'yet-another-react-lightbox/styles.css';
 
-// Import images directly to ensure proper bundling by webpack
-import img1 from '../assets/pics/1.jpg';
-import img2 from '../assets/pics/2.jpg';
-import img3 from '../assets/pics/3.jpg';
-import img4 from '../assets/pics/4.jpg';
-import img5 from '../assets/pics/5.jpg';
-import img6 from '../assets/pics/6.jpg';
-import img7 from '../assets/pics/7.jpg';
-import img8 from '../assets/pics/8.jpg';
-import img9 from '../assets/pics/9.jpg';
-import img10 from '../assets/pics/10.jpg';
-import img11 from '../assets/pics/11.jpg';
-
+// Use absolute paths which work consistently across environments
 const images = [
-  img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11
+  '/assets/pics/1.jpg',
+  '/assets/pics/2.jpg',
+  '/assets/pics/3.jpg',
+  '/assets/pics/4.jpg',
+  '/assets/pics/5.jpg',
+  '/assets/pics/6.jpg',
+  '/assets/pics/7.jpg',
+  '/assets/pics/8.jpg',
+  '/assets/pics/9.jpg',
+  '/assets/pics/10.jpg',
+  '/assets/pics/11.jpg'
 ];
 
 const breakpointColumnsObj = {
