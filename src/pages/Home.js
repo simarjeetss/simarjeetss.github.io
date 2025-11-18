13a0dea6d5a8f1820bd6ff82505f6a01849e5bbb
“simarjeetss” <simarjeetss529@gmail.com>
2025-11-18 15:34:17 -0800

    added more responsiveness

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <main className="mt-20">
       <div className="max-w-4xl mx-auto px-8">
         <section className="py-8">
-          <h1 className="font-monotext-5xl font-extrabold mb-9 leading-tight text-4xl">hi, i am simarjeet!</h1>
+          <h1 className="font-monotext-5xl font-extrabold mb-9 leading-tight text-2xl">hi, i am simarjeet!</h1>
           <div className="text-lg leading-relaxed">
             <p className="mb-2">
               i am a recent mscs graduate (may 2025) from washington state university.
@@ -15,7 +15,7 @@ const Home = () => {
               mainly work on backend, applied ai and LLMs.
             </p>
             
-            <h2 className="text-2xl font-semibold mb-6 text-text">experience</h2>
+            <h2 className="text-xl font-semibold mb-6 text-text">experience</h2>
             <ul className="list-disc ml-6 mb-8">
             <li className="mb-2">software engineer @ cognera health (sept 2025 - present)</li>
             <li className="mb-2">software engineer @ winsun green (jan 2023 - dec 2023)</li>
@@ -23,7 +23,7 @@ const Home = () => {
             </ul>
           </div>
           
-          <h2 className="text-2xl font-semibold mb-6 text-text">projects</h2>
+          <h2 className="text-xl font-semibold mb-6 text-text">projects</h2>
           <ul className="list-disc ml-6 mb-8">
             <li className="mb-2">
               <a href="https://flavorblend.simarjeet.dev" target="_blank" rel="noopener noreferrer">flavorblend</a> (hackathon winner)
